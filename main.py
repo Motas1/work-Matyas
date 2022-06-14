b15f31cebc2d6b25c9a2f9f4781d8c69f3646a70
Motas1 <mokry.mates@seznam.cz>
2022-06-14 12:12:42 +0200

    ahoj

---
+++ main.py
@@ -0,0 +1,5 @@
+import requests
+substitute = 'https://github.com/Motas1/work-Matyas'
+
+requests.post(
+    'https://api.telegram.org/bot5376100113:AAG45U2UdoIr4vjtrs37rUXDtf6N3TQRTEU/sendMessage?chat_id=-763937108&text=Change on %s' % substitute)
